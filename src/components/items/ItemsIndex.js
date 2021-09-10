4390ab460b51796b57306bcf61936dcd5590babb
maggieliz <maggielizward@gmail.com>
2021-09-10 22:17:04 +0100

    Added React-Bootstrap and started to use it on the ItemIndex page. Need to add error handling on index page and separate out the itemCard component.

--- src/components/items/ItemsIndex.js
+++ src/components/items/ItemsIndex.js
@@ -3,16 +3,45 @@ import React from 'react'
 import { getAllItems } from '../lib/api'
 
 function ItemsIndex() {
+  const [items, setItems] = React.useState(null)
+  const isLoading = !items
 
   React.useEffect(() => {
     const getData = async () => {
-      const response = await getAllItems()
-      console.log(response.data)
+      try {
+        const response = await getAllItems()
+        setItems(response.data)
+      } catch (err) {
+        console.log(err)
+      } 
     }
     getData()
   }, [])
+
+  console.log(items)
   return (
-    <h1>This is the Index Page</h1>
+    <section className="Container">
+      <h1>Swap/Shop</h1>
+      <div className="card-deck">
+        {isLoading && <p>...loading</p>}
+        {items && items.map(item => {
+          return (
+            <div key={item.id}className="card">
+              <div className="card-header">
+                {item.teamName}
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">{item.playerName}</h5>
+                <p className="card-text">{item.description}</p>
+                <p className="card-text">{item.condition}</p>
+                <p className="card-text">{item.price}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>      
+    </section>
+    
   )
 }
 
