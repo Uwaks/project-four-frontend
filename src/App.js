e551c2d5087bde9188009d092feaaf89b0aed0c5
Nnanna <nnanna.uwakwe@outlook.com>
2021-09-10 15:02:52 +0100

    Hooked up the connection, back & front end now communicating.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import axios from 'axios'
 
 function App() {
-  return <h1>Hello World</h1>
+  React.useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get('/api/items')
+      console.log(response.data)
+    }
+    getData()
+  }, [])
+
+  return (
+    <h1>This is the App</h1>
+  )
 }
 
 export default App
