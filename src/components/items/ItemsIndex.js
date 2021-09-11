42057f9a360f6763af630f8ee3a9f19e5a49b1df
maggieliz <maggielizward@gmail.com>
2021-09-11 10:27:08 +0100

    Got the bootstrap card working correctly.

--- src/components/items/ItemsIndex.js
+++ src/components/items/ItemsIndex.js
@@ -30,6 +30,7 @@ function ItemsIndex() {
               <div className="card-header">
                 {item.teamName}
               </div>
+              <img className="card-img-top" src={item.image} alt={item.playerName} />
               <div className="card-body">
                 <h5 className="card-title">{item.playerName}</h5>
                 <p className="card-text">{item.description}</p>
