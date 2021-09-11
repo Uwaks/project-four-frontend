d1697bf49b5481c86c596c709642e2837a1d91e4
Nnanna <nnanna.uwakwe@outlook.com>
2021-09-11 18:03:31 +0100

    Built the navbar with react bootstrap and started populating the items data.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react'
-import axios from 'axios'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import ItemsIndex from './components/items/ItemsIndex'
 import Home from './components/common/Home'
-import Navbar from './components/common/Navbar'
+import Header from './components/common/Header'
+import Footer from './components/common/Footer'
 
 function App() {
-  React.useEffect(() => {
-    const getData = async () => {
-      const response = await axios.get('/api/items')
-      console.log(response.data)
-    }
-    getData()
-  }, [])
 
   return (
     <BrowserRouter>
-      <Navbar />
+      <Header />
       <Switch>
         <Route exact path="/">
           <Home />
@@ -26,6 +19,7 @@ function App() {
           <ItemsIndex />
         </Route>
       </Switch>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/data/items.js
@@ -0,0 +1,162 @@
+export const items = [
+  {
+    player_name: 'Paul Stewart',
+    team_name: 'Liverpool',
+    image: 'https://www.soccerbible.com/media/57442/premier-league-1992-kits-barnes.jpg',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: 'Steven Gerrard',
+    team_name: 'Liverpool',
+    image: 'https://dnre29p915wg3.cloudfront.net/media/catalog/product/cache/1/image/750x/0dc2d03fe217f8c83829496872af24a0/l/i/liverpool-06-third-euro-gerrard_2_2_1.jpg',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: 'Nicky Barmby',
+    team_name: 'Manchester City',
+    image: 'https://www.soccerbible.com/media/57452/premier-league-1992-kits-mcfc.jpg',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: '',
+    team_name: 'Manchester City',
+    image: 'https://www.prodirectsoccer.com/ProductImages/Main/247301_Main_Thumb_0954085.jpg',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: 'Andrei Kanchelskis',
+    team_name: 'Manchester United',
+    image: 'https://www.soccerbible.com/media/57453/premier-league-1992-kits-mufc1.jpg',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: 'Dimitar Berbatov',
+    team_name: 'Manchester United',
+    image: 'https://www.oldfootballshirts.com/img/shirts/12/manchester-united-third-football-shirt-2008-2009-s_16980_2.jpg',
+    price: 99.99, 
+    description: '2008 - 2009 Manchester United third football shirt. Third Shirt Berbatov number 9, Champions League nameset and patch.',
+    condition: 'Good'
+  },
+  {
+    player_name: '',
+    team_name: 'Newcastle United',
+    image: 'https://www.prodirectsoccer.com/ProductImages/Main/228866_Main_0742595.jpg',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: '',
+    team_name: 'Newcastle United',
+    image: 'https://www.prodirectsoccer.com/ProductImages/Main/206175_Main_Thumb_0546542.jpg',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: '',
+    team_name: 'Norwich City',
+    image: '',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: '',
+    team_name: 'Norwich City',
+    image: '',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: '',
+    team_name: 'Southampton',
+    image: 'https://www.soccerbible.com/media/57458/premier-league-1992-kits-southampton.jpg',
+    price: , 
+    description: '',
+    condition: ''
+  },
+  {
+    player_name: 'Rasiak',
+    team_name: 'Southampton',
+    image: 'https://www.oldfootballshirts.com/img/shirts/40/southampton-home-football-shirt-2006-2007-s_14399_2.jpg',
+    price: , 
+    description: '',
+    condition: 'Match Worn'
+  },
+  {
+    player_name: 'Darren Bent',
+    team_name: 'Tottenham Hotspur',
+    image: 'https://www.oldfootballshirts.com/img/shirts/18/extra_football_shirt_10192_2.jpg',
+    price: 199.99, 
+    description: 'Match Worn Shirt by Darren Bent during English Premier League vs West Ham 29-12-09 and signed by the whole squad.',
+    condition: 'Match Worn'
+  },
+  {
+    player_name: 'Jurgen Klinsman',
+    team_name: 'Tottenham Hotspur',
+    image: 'https://cdn.shopify.com/s/files/1/0188/0122/products/IMG_20200311_110641_grande.jpg?v=1584386042',
+    price: 129.95, 
+    description: 'Official Umbro Tottenham Hotspur 3rd kit football shirt from the 1991/92 season. Complete with Klinsmann number 18 on the back of the shirt in official flock lettering.',
+    condition: 'Excellent'
+  },
+  {
+    player_name: 'Adrian Mariappa',
+    team_name: 'Watford',
+    image: 'https://www.oldfootballshirts.com/img/shirts/19/watford-away-football-shirt-2010-2011-s_39607_1.jpg',
+    price: 52.99, 
+    description: 'Watford away shirt 2010-11 season',
+    condition: 'Satisfactory'
+  },
+  {
+    player_name: 'Mich Hyde',
+    team_name: 'Watford',
+    image: 'https://www.oldfootballshirts.com/img/shirts/19/watford-home-football-shirt-1997-1998-s_17049_1.jpg',
+    price: 75.99, 
+    description: 'Watford home shirt 1997-98 season.',
+    condition: 'Good'
+  },
+  {
+    player_name: 'Papa Diop',
+    team_name: 'West Ham United',
+    image: 'https://dnre29p915wg3.cloudfront.net/media/catalog/product/cache/1/image/1500x/0dc2d03fe217f8c83829496872af24a0/w/e/west-ham-20-home-mw-diop-back.jpg',
+    price: 249.99, 
+    description: 'Very rare player shirt believed to have been issued to Diop for the Premier League match against Manchester United, played at the London Stadium on 5th December 2010.',
+    condition: 'Excellent'
+  },
+  {
+    player_name: 'Paolo Di Canio',
+    team_name: 'West Ham United',
+    image: 'https://dnre29p915wg3.cloudfront.net/media/catalog/product/cache/1/image/750x/0dc2d03fe217f8c83829496872af24a0/w/e/west-ham-99-home-di-canio_1_3_1_2_2.jpg',
+    price: 124.99, 
+    description: 'Home shirt as worn in Harry Redknapp\'s last season in charge and when Di Canio scored the famous scissor volley.',
+    condition: 'Very Good'
+  },
+  {
+    player_name: '',
+    team_name: 'Wolverhampton Wanderers',
+    image: 'https://dnre29p915wg3.cloudfront.net/media/catalog/product/cache/1/image/750x/0dc2d03fe217f8c83829496872af24a0/w/o/wolves-95-home_4_3_1_2_1_3_2_4_1_1_2_2_1_1_1_1_1.jpg',
+    price: 89.99, 
+    description: '',
+    condition: 'Very Good'
+  },
+  {
+    player_name: '',
+    team_name: 'Wolverhampton Wanderers',
+    image: 'https://dnre29p915wg3.cloudfront.net/media/catalog/product/cache/1/image/750x/0dc2d03fe217f8c83829496872af24a0/w/o/wolves-96-home-use-1_3_1_1_4_1_1_3_1_2_1_1_3_1_1_1_1_1_1_1.jpg',
+    price: 124.99, 
+    description: '',
+    condition: 'Excellent'
+  }
+]
