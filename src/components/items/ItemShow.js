73980765b8b89e9fc8a75de78337f5ca31730a0b
maggieliz <maggielizward@gmail.com>
2021-09-11 19:37:03 +0100

    Have a bare-bones ItemShow page. Needs styling and it needs the add to cart functionality hooked up.

--- src/components/items/ItemShow.js
+++ src/components/items/ItemShow.js
@@ -1,10 +1,58 @@
+import React from 'react'
 import { useParams } from 'react-router'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+
+import { getSingleItem } from '../lib/api'
 
 function ItemShow() {
-  const { itemId } = useParams
+
+  const { itemId } = useParams()
+  const [item, setItem] = React.useState(null)
+  const [isError, setIsError] = React.useState(false)
+  const isLoading = !item && !isError
+  
   console.log(itemId)
+
+  React.useEffect(()=> {
+    const getData = async () => {
+      try {
+        const response = await getSingleItem(itemId)
+        setItem(response.data)
+      } catch (err) {
+        setIsError(true)
+      }
+    }
+    getData()
+  },[itemId])
+
   return (
-    <h1> this is the item show page</h1>
+    <div>
+      {isError && <p>Sorry, something went wrong.</p>}
+      {isLoading && <p>...loading</p>}
+      {item && (
+        <Container fluid>
+          <Row>
+            <Col className="text-center">
+              <h1>{item.teamName}</h1>            
+            </Col>  
+          </Row>
+          <Row>
+            <Col>
+              <img src={item.image} alt={item.name} />
+            </Col>
+            <Col>
+              <h2>{item.playerName}</h2>
+              <h3>About</h3>
+              <p>{item.description}</p>
+              <p>Condition: {item.condition}</p>
+              <h2>£{item.price}</h2>
+              <Button>Add to cart</Button>
+            </Col>
+          </Row>
+        </Container>
+      )
+      } 
+    </div>    
   )
 }
 
--- src/components/lib/api.js
+++ src/components/lib/api.js
@@ -5,3 +5,7 @@ const baseURL = '/api'
 export function getAllItems() {
   return axios.get(`${baseURL}/items/`)
 }
+
+export function getSingleItem(itemId) {
+  return axios.get(`${baseURL}/items/${itemId}`)
+}
