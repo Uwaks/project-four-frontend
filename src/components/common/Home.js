e3f0869311ef0cbd952ea71d373c1d31b1e37700
maggieliz <maggielizward@gmail.com>
2021-09-13 21:53:48 +0100

    Trying to get recently added ItemCards onto the home page. The function seems to be working, but I can't get it to print to the page. Got the SimilarItems working on the ItemShow page.

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,7 +1,9 @@
+
 import React from 'react'
 
 import { Link } from 'react-router-dom'
-import { Card, Container, Jumbotron } from 'react-bootstrap'
+import { Container, Jumbotron } from 'react-bootstrap'
+import ItemCard from '../items/ItemCard'
 
 import { getAllItems } from '../lib/api'
 
@@ -10,6 +12,7 @@ function Home() {
   const [items, setItems] = React.useState('')
   const [isError, setIsError] = React.useState(false)
   const isLoading = !items && !isError
+  const recentlyAdded = []
 
   React.useEffect(() => {
     const getData = async () => {
@@ -23,6 +26,16 @@ function Home() {
     getData()
   }, [])
 
+
+  const filterItems = () => {
+    console.log(items)
+    items.sort((a, b) => (a.id < b.id))
+    recentlyAdded.push(items)
+    console.log('recently added', recentlyAdded)
+  }
+
+  items ? filterItems() : console.log('no items')
+
   return (
     <Container>
       <div className="row">
@@ -33,6 +46,22 @@ function Home() {
             <h1>SWAPxSHOP</h1>     
           </div>   
         </Jumbotron>
+      </div>
+      <div>
+        {items && (
+          recentlyAdded.map(item => {
+            item.map(card => {
+              return (
+                <div key={card.id}>
+                  <p>{card.playerName}</p>
+                  <Link key={card.id} to={`/items/${card.id}`}>
+                    <ItemCard card={ item } />
+                  </Link> 
+                </div>                
+              )
+            })  
+          })
+        )}
       </div>
     </Container>
   )
--- src/components/items/ItemShow.js
+++ src/components/items/ItemShow.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 
 import { getAllItems, getSingleItem } from '../lib/api'
+import ItemCard from './ItemCard'
 // import ItemCard from './ItemCard'
 
 function ItemShow() {
@@ -13,30 +15,23 @@ function ItemShow() {
   const [isError, setIsError] = React.useState(false)
   const isLoading = !item && !isError
   const [cart, setCart] = React.useState([])
+  const sameTeamArray = []
 
   React.useEffect(()=> {
     const getData = async () => {
       try {
         const response = await getSingleItem(itemId)
         setItem(response.data)
+        const res = await getAllItems()       
+        setItems(res.data)
       } catch (err) {
         setIsError(true)
       }
     }
     getData()
-  },[itemId])
+  }, [itemId])
 
-  // React.useEffect(() => {
-  //   const getData = async () => {
-  //     try {
-  //       const response = await getAllItems()
-  //       setItems(response.data)
-  //     } catch (err) {
-  //       setIsError(true)
-  //     }
-  //   }
-  //   getData()
-  // }, [])
+  
 
   // ** setting item into state & adding to local storage **
   const addToCart = () => {
@@ -46,29 +41,22 @@ function ItemShow() {
 
 
   // * Work to try and display similar items based on teamName
-  // let similarItems = []
-  // const findSimilarItems = () => {
-  //   similarItems = items.filter(kit => {
-  //     return kit.teamName === item.teamName
-  //   })
-  //   console.log(similarItems)
-  // }
-
-  // const teamMatchCheck = (arr1, arr2) => {
-  //   const matchingItems = []
-  //   const filteredArray = arr1.filter(kit => {
-  //     kit.teamName.includes(arr2[0]) || kit.teamName.includes(arr2[1]) || kit.teamName.includes(arr2[2])
-  //   })
-  //   filteredArray.map(element => {
-  //     matchingItems.push(element)
-  //   })
-  //   const uniqueMatchingItems = matchingItems.filter(uniqueKit => {
-  //     uniqueKit.teamName !== item.teamName
-  //   })
-  //   return uniqueMatchingItems.map(element => {
-  //     similarItems.push(element)
-  //   })
-  // }
+  const teamMatchCheck = (arr1, arr2) => {
+    const matchingTeams = []
+    const filteredArray = arr1.filter(kit => {
+      return kit.teamName.includes(arr2)
+    })
+    filteredArray.map(element => {
+      matchingTeams.push(element)
+    })
+    const uniqueMatchingTeams = matchingTeams.filter(kit => {
+      return kit.playerName !== item.playerName
+    })
+    uniqueMatchingTeams.map(element => {
+      return sameTeamArray.push(element)
+    })
+  }
+  items ? teamMatchCheck(items, item.teamName) : console.log('no items')
 
   return (
     <div>
@@ -96,11 +84,23 @@ function ItemShow() {
           </Row>
         </Container>
       )
-      } 
-      <p>You might also like</p>
-      <div className="card-deck">
-        {/* {items &&} */}
-      </div>
+      }
+      {items && (
+        <div>
+          <p>You might also like</p>
+          <div className="card-deck">
+            {sameTeamArray.map(kit => {
+              return (
+                <Link key={kit.id} to={`/items/${kit.id}/`}>
+                  <ItemCard item={ kit } />
+                </Link>
+              )
+            })
+            }
+          </div>
+        </div> 
+      )} 
+      
     </div>    
   )
 }
