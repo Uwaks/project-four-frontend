9c3aa85f907313559938be1b1f9d1043892f3fb7
maggieliz <maggielizward@gmail.com>
2021-09-13 19:23:52 +0100

    Added hero image to Home page. Need to start filtering items for home page.

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,8 +1,40 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+import { Card, Container, Jumbotron } from 'react-bootstrap'
+
+import { getAllItems } from '../lib/api'
+
 function Home() {
+  
+  const [items, setItems] = React.useState('')
+  const [isError, setIsError] = React.useState(false)
+  const isLoading = !items && !isError
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await getAllItems()
+        setItems(res.data)
+      } catch (err) {
+        setIsError(true)
+      }
+    }
+    getData()
+  }, [])
+
   return (
-    <h1>This is the Home Page</h1>
+    <Container>
+      <div className="row">
+        {isError && <p>Oops! Something went wrong 😢</p>}
+        {isLoading && <p>...loading</p>}       
+        <Jumbotron>      
+          <div className="jumbo-div">
+            <h1>SWAPxSHOP</h1>     
+          </div>   
+        </Jumbotron>
+      </div>
+    </Container>
   )
 }
 
