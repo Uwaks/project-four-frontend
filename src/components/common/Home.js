0f10cba348d4baa06f618a5f4476b051208aba78
Nnanna <nnanna.uwakwe@outlook.com>
2021-09-16 12:41:56 +0100

    fixed carousel text.

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -2,7 +2,7 @@
 import React from 'react'
 
 import { Link } from 'react-router-dom'
-import { Container, Jumbotron, Carousel, Row } from 'react-bootstrap'
+import { Container, Carousel, Row } from 'react-bootstrap'
 import ItemCard from '../items/ItemCard'
 
 import { getAllItems } from '../lib/api'
@@ -39,14 +39,6 @@ function Home() {
   
   return (
     <>
-      {/* <Jumbotron>      
-        <div className="jumbo-div">
-          <Link to="/items">
-            <h1>SWAPxSHOP</h1>
-          </Link>     
-        </div>   
-      </Jumbotron> */}
-
       <Carousel>
         <Carousel.Item>
           <img
@@ -54,9 +46,11 @@ function Home() {
             src="https://www.gannett-cdn.com/-mm-/7222ab99f5f32771edbbf8a0663d8c9407320dad/c=0-227-4455-2744/local/-/media/2016/12/16/Bergen/NorthJersey/636174848685852947-AP-457130363469.jpg?width=2560"
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h1>SWAPxSHOP</h1>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Carousel.Caption className="title-wrapper">
+            <Link className="title-text-link" to='/items'>
+              <h1 className="title-text">SWAPxSHOP</h1>
+            </Link>
+            <p>Post A Kit, Sell A Kit, Buy A Kit</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -65,9 +59,11 @@ function Home() {
             src="https://www.gannett-cdn.com/-mm-/147222021a4df3ce80871baf403b0602b2fc3b03/c=0-70-2292-1365/local/-/media/2015/06/21/USATODAY/USATODAY/635705006182852176-USP-SOCCER-WOMEN-S-WORLD-CUP-SPAIN-AT-COSTA-RICA-73669516.JPG?width=2560"
             alt="Second slide"
           />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <Carousel.Caption className="title-wrapper">
+            <Link className="title-text-link" to='/items'>
+              <h1 className="title-text">SWAPxSHOP</h1>
+            </Link>
+            <p>Post A Kit, Sell A Kit, Buy A Kit</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -76,9 +72,11 @@ function Home() {
             src="https://www.belfasttelegraph.co.uk/sport/football/premier-league/4fb90/34528347.ece/AUTOCROP/w1240/PANews%20BT_P-702dd505-ba02-456d-bf8b-d1682fea5deb_I1.jpg"
             alt="Third slide"
           />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          <Carousel.Caption className="title-wrapper">
+            <Link className="title-text-link" to='/items'>
+              <h1 className="title-text">SWAPxSHOP</h1>
+            </Link>
+            <p>Post A Kit, Sell A Kit, Buy A Kit</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
